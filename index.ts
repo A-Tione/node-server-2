81cc1f51c3a3570b951adba116a45ca6ea61fe40
a-tione <heyanggo@gmail.com>
2020-09-03 20:35:52 +0800

    add 基础结构

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 
 const server = http.createServer();
 
-server.on('request', ()=> {
+server.on('request', (request: IncomingMessage, response: ServerResponse)=> {
     console.log('有人请求了')
+    console.log(request.method);
+    console.log(request.url);
+    console.log(request.headers);
+    const array = []
+    request.on('data', chunk => {
+        array.push(chunk)
+    })
+    request.on('end', ()=> {
+        const body = Buffer.concat(array).toString();
+        console.log(body, 'body');
+        response.statusCode = 404
+        response.setHeader('xxx', 'yyy')
+        response.setHeader('Content-Type', 'image/img')
+        response.write('1\n')
+        response.end()
+    })
 })
 
 server.listen(8888)
